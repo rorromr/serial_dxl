62c80129059d2b73cb4521e1a5b80e10fb118d0b
Rodrigo Muñoz <rorro.mr@gmail.com>
2016-08-31 14:55:37 -0300

    logger and led test

--- SerialDXL.h
+++ SerialDXL.h
@@ -6,23 +6,12 @@
 #include "data_serialization.h"
 #include "variable.h"
 #include "mmap.h"
+#include "logger.h"
 #include <avr/sleep.h>
 #include <Arduino.h>
 
 #ifndef SerialDXL_h
 #define SerialDXL_h
-//------------------------------------------------------------------------------
-/**
- * Debug macros
- */
-#define DEBUG_SERIAL_DXL
-#ifdef DEBUG_SERIAL_DXL
-#define DEBUG_PRINT(...) Serial.print(__VA_ARGS__)
-#define DEBUG_PRINTLN(...) Serial.println(__VA_ARGS__)
-#else
-#define DEBUG_PRINT(...)
-#define DEBUG_PRINTLN(...)
-#endif
 //------------------------------------------------------------------------------
 /**
  * @class DeviceDXL
